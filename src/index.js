952b21591771a78f7204eec561af70e1bf73d29f
kshmotolokha <kostiantyn_shmotolokha@qollabe.com>
2023-04-16 00:02:07 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -12,34 +12,29 @@ const app = new PIXI.Application({
 
 const sprite1 = PIXI.Sprite.from(img);
 const sprite2 = PIXI.Sprite.from(img);
-
-sprite1.x = 100;
-sprite1.y = 100;
-
-sprite2.x = 100;
-sprite2.y = 100;
-sprite2.width = 100
-sprite2.height =100
-sprite2.anchor.set(0.5);
-
-app.stage.addChild(sprite1);
-app.stage.addChild(sprite2);
+const tween = gsap.to(sprite1, { x: 500, y: 100, duration: 1 });
+const sequence1 = gsap.timeline({ repeat: 0, onComplete: startMovement });
+const sequence2 = gsap.timeline({ repeat: -1 });
 
 function startMovement() {
     tween.restart();
 }
 
-const tween = gsap.to(sprite1, { x: 400, y: 100, duration: 1 });
-const sequence1 = gsap.timeline({ repeat: 0, onComplete: startMovement });
+sprite1.x = 100;
+sprite1.y = 100;
+sprite2.x = 700;
+sprite2.y = 100;
+sprite2.width = 100;
+sprite2.height =100;
+sprite2.anchor.set(0.5);
+
+app.stage.addChild(sprite1);
+app.stage.addChild(sprite2);
 
 sequence1.to(sprite1, { alpha: 1, duration: 1 })
-         .to(sprite2, { alpha: 1, duration: 1 });
-
-const sequence2 = gsap.timeline({ repeat: -1 });
 
 sequence1.eventCallback('onComplete', () => {
-	sequence2.to(sprite2, { duration: 0.5, alpha: 0.5  })
-         .to(sprite2,  { duration: 0.5, alpha: 1 });
+	sequence2.to(sprite2, { duration: 1, alpha: 0  })
   });
 
 sequence1.play();
